3da1231dcce77b27b0c2505ddb574ba35015cdc2
JKetelaar <jeroen@ketelaar.me>
2014-09-11 22:46:10 +0200

    Improved code of the TriangleSandwich

--- src/org/parabot/randoms/pkhonor/TriangleSandwich.java
+++ src/org/parabot/randoms/pkhonor/TriangleSandwich.java
@@ -4,8 +4,7 @@ import org.parabot.environment.api.utils.Time;
 import org.parabot.environment.scripts.framework.SleepCondition;
 import org.parabot.environment.scripts.randoms.Random;
 import org.rev317.min.api.methods.Inventory;
-import java.lang.Override;
-import java.lang.String;
+import org.rev317.min.api.wrappers.Item;
 
 /**
  * Created with IntelliJ IDEA.
@@ -14,30 +13,41 @@ import java.lang.String;
  * Time: 22:29
  */
 public class TriangleSandwich implements Random {
+
+    Item item;
+
     @Override
     public boolean activate() {
-        return Inventory.getCount(6963) > 0;
+        for (Item i : Inventory.getItems(6963)){
+            if (i != null){
+                this.item = i;
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public void execute() {
         //Drop Triangle Sandwich
-        Inventory.getItems(6963)[0].drop();
-        Time.sleep(new SleepCondition() {
-            @Override
-            public boolean isValid() {
-                return Inventory.getCount(6963) == 0;
-            }
-        },1500);
-
-        @Override
-        public String getName() {
-            return "Triangle Sandwich Handeler";
+        if (this.item != null) {
+            item.drop();
+            Time.sleep(new SleepCondition() {
+                @Override
+                public boolean isValid() {
+                    return Inventory.getCount(6963) == 0;
+                }
+            }, 1500);
         }
+    }
 
-        @Override
-        public String getServer() {
-            return "pkhonor";
-        }
+    @Override
+    public String getName() {
+        return "Triangle Sandwich Handler";
+    }
+
+    @Override
+    public String getServer() {
+        return "pkhonor";
     }
 }

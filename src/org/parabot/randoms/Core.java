df67c0dc1dd420612f9ff8e42cf246b9ad18621e
Piet Heeringa <pj_heeringa@hotmail.com>
2014-09-12 17:25:35 +0200

    Added Randoms to Core
    ^

--- src/org/parabot/randoms/Core.java
+++ src/org/parabot/randoms/Core.java
@@ -3,6 +3,8 @@ package org.parabot.randoms;
 import org.parabot.core.Context;
 import org.parabot.environment.scripts.randoms.Random;
 import org.parabot.randoms.pkhonor.Jail;
+import org.parabot.randoms.pkhonor.MysteriousOldMan;
+import org.parabot.randoms.pkhonor.SandwichLady;
 import org.parabot.randoms.pkhonor.TriangleSandwich;
 
 import java.util.ArrayList;
@@ -16,6 +18,8 @@ public class Core {
     public void init(String server){
         randoms.add(new Jail());
         randoms.add(new TriangleSandwich());
+        randoms.add(new SandwichLady());
+        randoms.add(new MysteriousOldMan());
 
         for (Random random : randoms){
             if (random.getServer().toLowerCase().equalsIgnoreCase(server.toLowerCase())){
